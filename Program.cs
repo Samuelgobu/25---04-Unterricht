ce50112098b2641ce0b7e1ed97536da08b0fa224
Qian Ma <katharina.mq@outlook.de>
2022-04-27 08:45:37 +0200

    ehhhhhh

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace _25___04_Unterricht
             //if (entscheidung == "j")
             //{
             //}
-            Random r = new Random();
+            //Random r = new Random();
             //while (true)
             //{
             //    int Zufallszahl = r.Next(1, 7);
@@ -50,36 +50,58 @@ namespace _25___04_Unterricht
             //double Erwartungswert = (double)Summe / (double)Anzahl;
             //Console.WriteLine("Erwartungswert:" + Erwartungswert);
             //Cast: mit (double)Summe wird an dieser Stelle Summe als double verwndet
-            int Summe = 0;
-            Console.WriteLine("Wie viel wollen Sie die Würfel werfen?");
-            int Anzahl = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < Anzahl; i++)
+
+
+            //int Summe = 0;
+            //Console.WriteLine("Wie viel wollen Sie die Würfel werfen?");
+            //int Anzahl = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < Anzahl; i++)
+            //{
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //    Console.WriteLine((i + 1) + ":");
+            //    int Zufallszahl = r.Next(1, 3);
+            //    if (Zufallszahl == 1)
+            //    {
+            //        Zufallszahl = 6;
+            //        Console.WriteLine(Zufallszahl);
+            //    }
+            //    else if (Zufallszahl == 2)
+            //    {
+            //        Zufallszahl = r.Next(1, 6);
+            //        Console.WriteLine(Zufallszahl);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Falsche Angabe");
+            //    }
+            //    Summe += Zufallszahl;
+            //    Console.WriteLine("Zahl=" + Zufallszahl + " Summe=" + Summe);
+
+            //}
+            //double Erwartungswert = (double)Summe / (double)Anzahl;
+            //Console.WriteLine("Erwartungswert:" + Erwartungswert);
+
+            // WEITERE ANWENDUNGEN VON RANDOM
+            Random r = new Random(21);
+            for (int i = 0; i < 100; i++) 
             {
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine((i + 1) + ":");
-                int Zufallszahl = r.Next(1, 3);
-                if (Zufallszahl == 1)
-                {
-                    Zufallszahl = 6;
-                    Console.WriteLine(Zufallszahl);
-                }
-                else if (Zufallszahl == 2)
-                {
-                    Zufallszahl = r.Next(1, 6);
-                    Console.WriteLine(Zufallszahl);
-                }
-                else
-                {
-                    Console.WriteLine("Falsche Angabe");
-                }
-                Summe += Zufallszahl;
-                Console.WriteLine("Zahl=" + Zufallszahl + " Summe=" + Summe);
-
+                Console.WriteLine(100*r.NextDouble());
             }
-            double Erwartungswert = (double)Summe / (double)Anzahl;
-            Console.WriteLine("Erwartungswert:" + Erwartungswert);
 
+            // ZAHLENSYSTEME
+            // Dezimalsystem ( 10en-System)
+            // 1324 = 1*10^3 + 3*10^2 + 2*10^1 + 4*10^0
+            // -------------------------------------------------
+            // Binarsystem (2en-System)
+            // b = 2, z(i) E {0,1}
+            // 1011 = 1*2^3 + 0*2^2 + 1*2^1 + 1*2^0 
+            //      = 11(10)
+            // 73(10 (normal))-> 1001001(2) (1*2^6 + 1*2^3 + 1*2^0)
+            //---------------------------------------------------------
+            // Mathoben
+            //    Math.Pow(3,2) = 3^2=9
+            //    y=f(x)=3x+1
 
 
 
